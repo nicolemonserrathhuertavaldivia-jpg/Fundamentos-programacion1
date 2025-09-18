c2d05236cc0b29605aff66a6f4813f73b63b5876
nicolemonserrathhuertavaldivia-jpg <nicolemonserrathhuertavaldivia@gmail.com>
2025-09-18 07:56:08 -0600

    lista1

---
+++ Unidad 2/lista1.py
@@ -0,0 +1,17 @@
+#vamos a declarar variables
+lista = ["Juan","Ivan","Adrian","Jair"]
+print (lista)
+#imprimir lista
+print (lista[2])
+#sirve para agregar un item al final de la lista
+lista.append("Alexa")
+print (lista)
+lista.insert (1,"Javier")
+print (lista)
+#funciona para eliminar el ultimo que se ingreso en la lista
+lista.pop()
+print (lista)
+lista.remove("Juan")
+print(lista)
+lista.pop(1)
+print (lista)
