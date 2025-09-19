a232c39a3fb55cf7d4411775c1c8443ec712a9f9
nicolemonserrathhuertavaldivia-jpg <nicolemonserrathhuertavaldivia@gmail.com>
2025-09-19 07:31:35 -0600

    lista 2

---
+++ Unidad 2/lista2.py
@@ -0,0 +1,21 @@
+#Se crea una lista llamada lista_compras
+lista_compras = []
+
+print("🛒 Lista de Compras 🛒")
+#Agregar producto
+producto1 = input(" Agrega el primer producto: ")
+lista_compras.append(producto1)
+
+#Agregar producto
+producto2 = input("Agrega el segundo producto: ")
+lista_compras.append(producto2)
+
+producto3 = input("Agrega el tercer producto: ")
+lista_compras.append(producto3)
+
+print("\n📌 Tu lista de compras es:")
+for producto in lista_compras:
+    print(f"- {producto}")
+
+print("\n✅ ¡Lista creada con éxito!")
+#https://www.webfx.com/tools/emoji-cheat-sheet/
